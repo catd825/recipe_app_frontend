d26534472423de74c6e2a0c13f9c264afb98027e
catd825 <catd825@gmail.com>
2021-03-25 10:31:29 -0400

    updated ffetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
 import RecipeContainer from './Containers/RecipeContainer'
 
 function App() {
 
 
-  const fetchFavorites = async () => {
-    const response = await fetch('http://localhost:3000/favorite_recipes');
-    const json = await response.json();
-    console.log("favorites", json)
-  };
+  // const fetchFavorites = async () => {
+  //   const response = await fetch('http://localhost:3000/favorite_recipes');
+  //   const json = await response.json();
+  //   console.log("favorites", json)
+  // };
 
 
   // fetchFavorites();
--- src/Containers/RecipeContainer.js
+++ src/Containers/RecipeContainer.js
@@ -1,17 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function RecipeContainer() {
 const [recipes, recipeState] = useState([]);
+const recipeUrl = 'http://localhost:3000/recipes'
 
-useEffect(async () => {
-    const fetchRecipes = await fetch('http://localhost:3000/recipes').then(res => res.text())
-    recipeState(fetchRecipes)
-}, []);
+useEffect( () => {
+    const fetchRecipes = async  () => {
+        const response = await fetch(recipeUrl)
+        const json = await response.json();
 
+        recipeState(json);
+    };
+    fetchRecipes();
+  }, []);
+
+  console.log(recipes.length)
   return (
-      <div>
-          {recipes}
-      </div>
+    <div>
+      {recipes.length !== 0 ? recipes.map(data => <div><img alt="img" style={{ height: '250px'}}src={data.img_url} /> {data.title} </div>) : null}
+    </div>
   )
 }
 
