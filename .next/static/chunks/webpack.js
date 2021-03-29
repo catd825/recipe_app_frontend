303b4ccf15ed400c70bfbe7d03d23f0f04916abd
catd825 <60204063+catd825@users.noreply.github.com>
2021-03-29 12:12:49 -0400

    extra commit for pages-manifest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "77da659a77657fb49471";
+/******/ 	var hotCurrentHash = "c004a5923ca8e8aab947";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
