578265d6bafa29ec8b2d23700ff412b55a521683
catd825 <60204063+catd825@users.noreply.github.com>
2021-03-29 21:15:51 -0400

    mandatory commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c004a5923ca8e8aab947";
+/******/ 	var hotCurrentHash = "31d805f55b67a0e88538";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
